9c0e74f45bcf2c2f14186c5df543ad6aae1712de
BUHOCORP02 <BUHOCORP02@DESKTOP-1MRU571>
2018-02-27 11:59:02 -0500

    find Proforma x producto

--- MVC/Model/ProformaDaoImp.php
+++ MVC/Model/ProformaDaoImp.php
@@ -21,7 +21,16 @@ class ProformaDaoImp {
         $conn = (new C_MySQL())->open();
         $banderapag = ($params["top"] > 0 ) ? "limit " . $params['top'] . " offset " . $params['pag'] : "";
         //where estado = 'ACT'
-        $sql = "select SQL_CALC_FOUND_ROWS * from viewProforma order by id desc $banderapag ;";
+//        $sql = "select SQL_CALC_FOUND_ROWS * from viewProforma order by id desc $banderapag ;";
+        $sql = "select `p`.`id` AS `id`,`p`.`codigo` AS `codigo`,`p`.`fecha` AS `fecha`,`p`.`idcliente` AS `idcliente`,`p`.`ganancia` AS `ganancia`,`p`.`envio` AS `envio`,`p`.`idgarantia` AS `idgarantia`,`c`.`nombres` AS `nombres`,`g`.`descripcion` AS `garantia` ,
+                count(dtp.id) as productos
+                from ((`proforma` `p` 
+                join `cliente` `c` on((`c`.`id` = `p`.`idcliente`))) 
+                join `garantia` `g` on((`g`.`id` = `p`.`idgarantia`)))
+                join detalleproforma dtp on dtp.idproforma = p.id
+                join producto prod on prod.id = dtp.idproducto
+                where prod.descripcion like  CONCAT('%','". $params["buscar"] ."','%')  
+                GROUP BY p.id";
 
         $list = C_MySQL::returnListAsoc($conn, $sql);
         $count = C_MySQL::row_count($conn);
